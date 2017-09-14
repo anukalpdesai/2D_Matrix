8d3037491bb3514fda41cfbec7feabdd46232739
anukalp <anukalp.desai@gmail.com>
2017-09-14 12:45:14 +0530

    added matrix functions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 #include "matrix_functions.cpp"
-#define ROW 4
-#define COL 4
+#include "matrix_constants.h"
 using namespace std;
 
 
 int main()
 {
-    int matrix[ROW][COL] = {0};
-    print_matrix(matrix, ROW, COL);
+    int row = 4, col = 4, num;
+    int matrix[ROW][COL] =  {{2, 8, 12, 16}, {3, 9, 13, 18},
+                             {7, 14, 15, 20}, {11, 19, 25, 28}
+                            };
+
+    print_matrix(matrix, row, col);
+
+    PRINT("Enter number to search in matrix: ");
+    scanf("%d", &num);
+    if(search_number(matrix, num, row, col) == 1)
+    {
+        PRINT("Number Found.");
+    }
+    else
+    {
+        PRINT("Number Not Found.");
+    }
+
+
 }
--- matrix_functions.cpp
+++ matrix_functions.cpp
@@ -1,13 +1,27 @@
 #include<stdio.h>
+#include"matrix_constants.h"
 
-void print_matrix(int (*matrix)[COL], int row, int col)
+void print_matrix(int matrix[ROW][COL], int row, int col)
 {
     for(int i=0; i<row; i++)
     {
         for(int j=0; j<col; j++)
         {
-            printf("%d ", matrix[i][j]);
+            printf("%3d ", matrix[i][j]);
         }
         printf("\n");
     }
+}
+
+int search_number(int matrix[ROW][COL], int num, int row, int col)
+{
+    for(int i=0; i<row; i++)
+    {
+        for(int j=0; j<col; j++)
+        {
+            if(matrix[i][j] == num)
+                return 1;
+        }
+    }
+    return 0;
 }
