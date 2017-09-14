05d70609938e3d6239f18f088e652527015bcaa4
anukalp <anukalp.desai@gmail.com>
2017-09-14 12:48:29 +0530

    modified .gitignore and matrix_constants

---
+++ matrix_constants.h
@@ -0,0 +1,13 @@
+#ifndef MATRIX_CONSTANTS_H_INCLUDED
+#define MATRIX_CONSTANTS_H_INCLUDED
+
+#define ROW 100
+#define COL 100
+
+#define PRINT(msg) printf("\n%s\n", msg);
+
+void print_matrix(int matrix[ROW][COL], int row, int col);
+int search_matrix(int matrix[ROW][COL], int num, int row, int col);
+
+
+#endif // MATRIX_CONSTANTS_H_INCLUDED
